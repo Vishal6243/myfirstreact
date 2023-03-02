d43623c32f0446a50d857b59c534310cbb9ee637
Gaurav Prajapati <mt_063@outlook.com>
2023-03-02 19:10:01 +0530

    footer changes

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return(
         <>
         <footer className='bg-light text-center py-2'>
-            <p className='m-0'>&#169; 2023 Responsive. All Right Reserved | Terms and Condition</p>
+            <p className='m-0'>&#169; 2023 Vnayak. All Right Reserved | Terms and Condition</p>
         </footer>
         </>
     )
